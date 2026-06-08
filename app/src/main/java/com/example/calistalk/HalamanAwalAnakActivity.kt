458046dc3ed64960ad002d5a981877fc5913257c
Noviyanti-Agustina-Putri <noviyantiagustinaputri@gmail.com>
2026-06-08 08:06:15 +0800

    edit beberapa fitur

--- app/src/main/java/com/example/calistalk/HalamanAwalAnakActivity.kt
+++ app/src/main/java/com/example/calistalk/HalamanAwalAnakActivity.kt
@@ -12,11 +12,12 @@ class HalamanAwalAnakActivity : AppCompatActivity() {
 
         setContentView(R.layout.halaman_awal_anak)
 
-
-
         val cardMenghitung = findViewById<CardView>(R.id.cardMenghitung)
         val cardMenulis = findViewById<CardView>(R.id.cardMenulis)
         val cardMiniGameBicara = findViewById<CardView>(R.id.cardGame)
+        val cardMendengar = findViewById<CardView>(R.id.cardMendengar)
+        val cardPengucapan = findViewById<CardView>(R.id.cardPengucapan)
+
 
         cardMenghitung.setOnClickListener {
             startActivity(
@@ -30,10 +31,25 @@ class HalamanAwalAnakActivity : AppCompatActivity() {
             )
         }
 
-        // 2. Tambahkan fungsi klik untuk pindah ke MiniGameBicaraActivity
         cardMiniGameBicara.setOnClickListener {
             startActivity(
                 Intent(this, MiniGameBicaraActivity::class.java)
+            )
+        }
+
+        cardMendengar.setOnClickListener {
+            startActivity(
+                Intent(this, PercobaanMendengarActivity::class.java)
+            )
+        }
+        cardPengucapan.setOnClickListener {
+            startActivity(
+                Intent(this, BelajarPengucapanActivity::class.java)
+            )
+        }
+        cardPengucapan.setOnClickListener {
+            startActivity(
+                Intent(this, BelajarPengucapanActivity::class.java)
             )
         }
     }
